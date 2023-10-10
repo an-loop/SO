0d4e00c17037bd55edb7cffa0651d01b0c609d09
Filipecorr34 <filipecorrea13976@gmail.com>
2023-10-09 22:35:56 -0300

    Codigo em Assembly

---
+++ trabalho1/Assembly.asm
@@ -0,0 +1,56 @@
+section .data
+    prompt db "Digite um número inteiro: ", 0
+    even_msg db "é par.", 10, 0
+    odd_msg db "é ímpar.", 10, 0
+
+section .bss
+    num resd 1
+
+section .text
+    global _start
+
+_start:
+    ; Escreve a mensagem de prompt na tela
+    mov eax, 4 ; sys_write
+    mov ebx, 1 ; stdout
+    mov ecx, prompt ; endereço da string
+    mov edx, 27 ; tamanho da string
+    int 0x80 ; chama a syscall
+
+    ; Lê o número digitado pelo usuário
+    mov eax, 3 ; sys_read
+    mov ebx, 0 ; stdin
+    mov ecx, num ; endereço do buffer de leitura
+    mov edx, 4 ; tamanho do buffer de leitura (4 bytes para um inteiro)
+    int 0x80 ; chama a syscall
+
+    ; Verifica se o número é par ou ímpar e escreve o resultado na tela
+    mov eax, [num]
+    and eax, 1 ; verifica o bit menos significativo (1 para ímpar, 0 para par)
+    jz even ; se for zero (par), pula para a label "even"
+    
+odd:
+    ; Escreve a mensagem "ímpar" na tela
+    mov eax, 4 ; sys_write
+    mov ebx, 1 ; stdout
+    mov ecx, odd_msg ; endereço da string "ímpar"
+    mov edx, 12 ; tamanho da string "ímpar"
+    int 0x80 ; chama a syscall
+    
+exit:
+    ; Sai do programa
+    mov eax, 1 ; sys_exit
+    xor ebx, ebx ; código de saída (zero indica sucesso)
+    int 0x80 ; chama a syscall
+
+even:
+    ; Escreve a mensagem "par" na tela
+    mov eax, 4 ; sys_write
+    mov ebx, 1 ; stdout
+    mov ecx, even_msg; endereço da string "par"
+    mov edx, 9; tamanho da string "par"
+    int 0x80 ; chama a syscall
+    
+ int 0x80 ; chama a syscall
+    
+jmp exit
